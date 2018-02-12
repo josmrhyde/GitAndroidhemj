c03a1ddb974a12ced87a0c0fdf8c0541df25439b
josmrhyde <iherjos@comunidad.unam.mx>
2018-02-11 18:27:50 -0600

    tercera modificacion

--- app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
+++ app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
@@ -7,21 +7,29 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private  Button boton;
-    private TextView txtVista;
-
+    private  Button boton, boton2;
+    private TextView txtVista, txtVista2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         boton = findViewById(R.id.button);
+        boton2 = findViewById(R.id.button2);
         txtVista = findViewById(R.id.textView);
+
 
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtVista.setText("Hola git hub");
+                txtVista.setText("Bienvenido a Github");
+            }
+        });
+
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtVista.setText("Bienvenido al Diplomado Android");
             }
         });
     }
